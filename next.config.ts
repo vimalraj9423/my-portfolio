4aa1a76b2e71a90f06e1992fe4c7c16d84e7073f
vimalraj9423 <vimalraj9423@gmail.com>
2025-04-14 23:21:32 +0530

    export option

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/my-portfolio", // Add your repository name
+  output: "export"
 };
 
 export default nextConfig;
