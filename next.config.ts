b2d966728b0116d4c3f2327ac19c3d7223a1cebb
vimalraj9423 <vimalraj9423@gmail.com>
2025-04-14 23:13:08 +0530

    github actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/my-portfolio", // Add your repository name
 };
 
 export default nextConfig;
