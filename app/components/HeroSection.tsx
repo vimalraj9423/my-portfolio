910df4d6d4c303de70634e24effade3fe192562e
vimalraj9423 <vimalraj9423@gmail.com>
2025-04-20 16:22:43 +0530

    update style

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 function HeroSection(){
     return <div className="flex flex-col-reverse md:flex-row h-[var(--hero-section)] px-10 md:px-20 py-10 md:py-14 gap-10">
         <div className="md:flex-1">
-    <div className="flex  flex-col justify-end h-full md:py-[55px] font-(family-name:--font-raleway) ">
+    <div className="flex  flex-col justify-end h-full pb-[55px] md:py-[55px] font-(family-name:--font-raleway) ">
             <h2 className="text-3xl font-semibold">Hello, I&apos;m</h2>
             <h1 className="text-4xl font-bold">Vimal <span className="text-[var(--primary-2)]">Raj V</span> </h1>
             <h6 className="text-base font-semibold">Front-End Developer specializing in React.js, Tailwind CSS & GitHub Actions. Crafting seamless UI experiences with scalable solutions.</h6>
